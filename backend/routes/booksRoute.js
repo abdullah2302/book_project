000cd3e68b603df20eab31d651502359302408aa
HP <abdullahbhatti8776@gmail.com>
2026-08-11 13:09:36 +0500

    feat: add morgan for logging middleware and update dependencies

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const { getAllBooks,getBookById, addBook, updateBook,deleteBook} = require("../controller/userController");
 const {checkUniqueTitle} = require("../middleware/middlewareBook");
 
 const router = express.Router();
-
+router.use(morgan("dev"));
 router.route("/").get(getAllBooks).post(checkUniqueTitle, addBook);
 
 router.route("/:id").get(getBookById).put(updateBook).delete(deleteBook); 

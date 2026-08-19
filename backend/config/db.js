afc94c6d0280cb720fe014e4db686159db1c0bbc
HP <abdullahbhatti8776@gmail.com>
2026-08-19 10:33:12 +0500

    refactor: simplify MongoDB connection logic with async/await

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,14 +5,11 @@ dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
 
 export async function connectDB() {
-    mongoose
-        .connect(MONGO_URI)
-        .then(() => {
-            console.log("MongoDB connected");
-        })
-        .catch((error) => {
-            console.log("MongoDB connection error:", error);
-        });
-};
-
-
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.log("MongoDB connection error:", error);
+        process.exit(1); 
+    }
+}

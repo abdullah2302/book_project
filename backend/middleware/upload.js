63e77013923a03d0d15a9be05b5c82237d9db1f6
HP <abdullahbhatti8776@gmail.com>
2026-08-19 11:25:43 +0500

    refactor: modularize file upload handling with multer configuration and middleware

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,21 +1,6 @@
-import multer from 'multer';
+import {upload} from '../config/multer.js';
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
-});
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true);
-    } else {
-        cb(new Error('Invalid file type'), false);
-    }
+export const uploadMiddleware = (req, res, next) => {
+    const uploadSingle = upload.single('coverImage');
+    uploadSingle(req, res, next);
 };
-
-export const upload = multer({ storage, fileFilter });
--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import { getAllBooks, getBookById, createBook, updateBook, deleteBook } from "../controller/bookController.js";
 import { checkUniqueTitle } from "../middleware/middlewareBook.js";
 import { bookValidationRules } from "../validator/bookValidator.js";
-import {upload} from "../middleware/upload.js";
+import { uploadMiddleware } from "../middleware/upload.js";
 
 
 const router = express.Router();
@@ -11,11 +11,11 @@ const router = express.Router();
 router.use(morgan("dev"));
 router.route("/")
       .get(getAllBooks)
-      .post(upload.single('coverImage'), bookValidationRules, checkUniqueTitle, createBook);
+      .post(uploadMiddleware, bookValidationRules, checkUniqueTitle, createBook);
 
 router.route("/:id")
       .get(getBookById)
-      .put(upload.single('coverImage'), bookValidationRules, checkUniqueTitle, updateBook)
+      .put(uploadMiddleware, bookValidationRules, checkUniqueTitle, updateBook)
       .delete(deleteBook);
 
 export default router;
